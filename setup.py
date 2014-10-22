e260400684fc3efa6c30c1be72e483d6b0263a42
maximebf <maxime.bouroumeau@gmail.com>
2014-10-23 00:32:27 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,6 @@
 from setuptools import setup, find_packages
 
 
-def desc():
-    with open("README.md") as f:
-        return f.read()
-
-def reqs():
-    with open('requirements.txt') as f:
-        return f.read().splitlines()
-
 setup(
     name='frasco-admin',
     version='0.1',
@@ -17,15 +9,21 @@ setup(
     author='Maxime Bouroumeau-Fuseau',
     author_email='maxime.bouroumeau@gmail.com',
     description="Admin interface for Frasco",
-    long_description=desc(),
     packages=find_packages(),
-    include_package_data=True,
+    package_data={
+        'frasco_admin': [
+            'blueprint/static/*.css',
+            'blueprint/static/*.js',
+            'blueprint/templates/admin/*.html',
+            'macros.html']
+    },
     zip_safe=False,
     platforms='any',
-    install_requires=reqs() + [
-        'frasco',
-        'frasco-bootstrap',
-        'frasco-users',
-        'frasco-users-acl'
+    install_requires=[
+        # 'frasco',
+        # 'frasco-bootstrap',
+        # 'frasco-users',
+        # 'frasco-users-acl',
+        'inflection'
     ]
 )
